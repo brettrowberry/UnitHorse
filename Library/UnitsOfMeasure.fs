423340df025e33c9e41576e36ff4040a93e80fbf
Brett Rowberry <brett@Bretts-MacBook-Pro.local>
2019-04-12 21:09:57 -0500

    added UnitsOfMeasure.fs to show why not a good fit

---
+++ Library/UnitsOfMeasure.fs
@@ -0,0 +1,31 @@
+module UnitsOfMeasure
+// https://fsharpforfunandprofit.com/posts/correctness-type-checking/
+
+type [<Measure>] m
+type [<Measure>] mm =
+   static member ToMeters(millimeters : float<mm>) : float<m> = 
+      millimeters * 1e-3<m/mm>
+
+let meters = 1.0<m>
+let millimeters = 1e3<mm>
+let millimetersInMeters = mm.ToMeters(millimeters)
+
+meters + millimeters // can't mix and match!
+
+// A B, n = 2
+// A->B
+// B->A
+// n(n-1) -> 2
+
+//A B C, n = 3
+// A->B, A->C
+// B->A, B->C
+// C->A, C->B
+// n(n-1) -> 6
+
+//A B C D, n = 4
+// A->B, A->C, A->D
+// B->A, B->C, B->D
+// C->A, C->B, C->D
+// D->A, D->B, D->C
+// n(n-1) -> 12
